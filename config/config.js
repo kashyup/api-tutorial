472bcb5202927e661d97b2387badbc0a7c1412b3
kashyup <rohitkashyap69489@gmail.com>
2022-03-09 20:37:18 +0530

    mongo-db

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const env = require("./env.json");
 
 exports.config = function() {
-    const node_env = process.env.NODE_ENV||"dev";
+    const node_env = process.argv[2];
     return env[node_env];
 }
